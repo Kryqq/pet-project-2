6683e9faae5f931292d44823888ce255bfc9906f
Dmitry Andreyev <Krygliy.ya@gmail.com>
2023-09-20 22:22:59 +0700

    donefunc

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const emptyList = document.querySelector('#emptyList');
 
 taskMenuForm.addEventListener('submit', addTask);
 taskList.addEventListener('click', deleteTask);
+taskList.addEventListener('click', doneTask);
 
 function addTask(event) {
    event.preventDefault();
@@ -17,8 +18,8 @@ function addTask(event) {
    const taskHTML = `
   <li id="taskList">
    <span id="taskListId">${taskText}</span>
-   <button  "btn-action"><img id = 'imageId_list' src="#" alt="Выполнено" /></button
-   ><button  data-action = 'delete'"btn-action"><img id = 'imageId_list'src="#" alt="Удалить" /></button>
+   <button data-action = 'done' class = "btn-action" ><img id = 'imageId_list' src="#" alt="Выполнено" /></button
+   ><button  data-action = 'delete' class = "btn-action"><img id = 'imageId_list'src="#" alt="Удалить" /></button>
 </li>`;
 
    taskList.insertAdjacentHTML('beforeend', taskHTML);
@@ -33,11 +34,19 @@ function addTask(event) {
 
 function deleteTask(event) {
    if (event.target.dataset.action === 'delete') {
-	 const parentNode = event.target.closest('li')
-	parentNode.remove()
-
+      const parentNode = event.target.closest('li');
+      parentNode.remove();
    }
    if (taskList.children.length === 1) {
-	emptyList.classList.remove('none');
-  }
+      emptyList.classList.remove('none');
+   }
+}
+
+function doneTask(event) {
+   if (event.target.dataset.action === 'done') {
+      const parentNode = event.target.closest('li');
+      const taskTitle = parentNode.querySelector('#taskListId');
+      taskTitle.classList.toggle('task-title-done');
+      console.log(taskTitle);
+   }
 }
